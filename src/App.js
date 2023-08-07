e6f735e35d3c943f17f78ab7a77e05d203d1b177
Vanessa <vanessambeattie@gmail.com>
2023-08-07 13:37:49 -0500

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import {Routes, Route} from 'react-router-dom'
+import Main from './pages/Main'
+import Currencies from './pages/Currencies'
+import Price from './pages/Price'
+import Nav from "./components/Nav";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Routes>
+          <Route path='/' element={ <Main /> } />
+          <Route path='/currencies' element={ <Currencies /> } />
+          <Route path="/price/:symbol" element={<Price/>}/>
+      
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Price.js
@@ -0,0 +1,52 @@
+import {useState, useEffect} from "react";
+import {useParams} from "react-router-dom"
+
+export default function Price (props) {
+  // Our api key from coinapi.io
+  
+  // Grabbing the Currency symbol from the URL Params
+  const params = useParams()
+  const symbol = params.symbol
+  // Using the other two variables to create our URL
+  const url = `http://rest.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${process.env.REACT_APP_COINAPI_KEY}`;
+
+  //state to hold the coin data
+  const [coin, setCoin] = useState("null");
+
+  //function to fetch coin data
+   const getCoin = async () => {
+    try{
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setCoin(data);
+    } catch(e){
+      console.error(e)
+    }
+  };
+
+  // useEffect to run getCoin when component mounts
+  useEffect(() => {
+    getCoin();
+  }, []);
+
+  // loaded function for when data is fetched
+  const loaded = () => {
+    return (
+      <div>
+        <h1>
+          {coin.asset_id_base}/{coin.asset_id_quote}
+        </h1>
+        <h2>{coin.rate}</h2>
+      </div>
+    );
+  };
+
+  // Function for when data doesn't exist
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+
+  // if coin has data, run the loaded function, otherwise, run loading
+  return coin && coin.rate ? loaded() : loading();
+};
